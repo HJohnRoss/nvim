9d17082d5bc8b8ef877944f430e83536bda5002f
John Ross <rossjohn936@gmail.com>
2025-01-15 14:06:53 -0800

    autocomplete

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -16,4 +16,13 @@ return {
 		end
 	},
 
+	{
+		"hrsh7th/nvim-cmp", 
+		dependencies = { "saadparwaiz1/cmp_luasnip", },
+		config = require("plugins.overrides.autocompletions").cmpsetup,
+	},
+	{ "hrsh7th/cmp-buffer",  },
+	{ "hrsh7th/cmp-path",  },
+	{ "hrsh7th/cmp-cmdline",  },
+	{ "hrsh7th/cmp-nvim-lua", },
 }
