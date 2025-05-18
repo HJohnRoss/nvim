86fcf5c9ea5294082d944facf0db37668342bee3
HJohnRoss <rossjohn936@gmail.com>
2025-05-18 11:54:37 -0700

    cursor annoyance

--- init.lua
+++ init.lua
@@ -3,13 +3,16 @@ vim.api.nvim_create_augroup('RazorFileType', { clear = true })
 
 -- Create an autocommand to set the filetype to razor for .razor files
 vim.api.nvim_create_autocmd(
-  { 'BufRead', 'BufNewFile' },      -- Events as a list
+  { 'BufRead', 'BufNewFile' }, -- Events as a list
   {
-    pattern = '*.razor',            -- Match files with .razor extension
+    pattern = '*.razor', -- Match files with .razor extension
     command = 'set filetype=razor', -- Command to run
-    group = 'RazorFileType',        -- Attach to the 'RazorFileType' group
+    group = 'RazorFileType', -- Attach to the 'RazorFileType' group
   }
 )
+
+vim.opt.guicursor = 'n-v-c:block,i-ci-ve:block,r-cr:block,o:hor20'
+
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
@@ -228,7 +231,7 @@ require('lazy').setup({
   -- Then, because we use the `opts` key (recommended), the configuration runs
   -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
 
-  {                     -- Useful plugin to show you pending keybinds.
+  { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     opts = {
@@ -309,7 +312,7 @@ require('lazy').setup({
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -417,7 +420,7 @@ require('lazy').setup({
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
-      { 'j-hui/fidget.nvim',    opts = {} },
+      { 'j-hui/fidget.nvim', opts = {} },
 
       -- Allows extra capabilities provided by blink.cmp
       'saghen/blink.cmp',
